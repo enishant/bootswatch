b460b338c45298aae3814a4de983ae61f57aa536
Nishant Vaity <enishant@gmail.com>
2017-06-08 18:26:15 +0530

    Added bootswatch.php and bootswatch theme css

---
+++ bootswatch.php
@@ -0,0 +1,21 @@
+<?php
+$content = file_get_contents('https://bootswatch.com/api/3.json');
+if(json_decode($content) != null) {
+	$bootswatch = json_decode($content);
+	if(isset($bootswatch->themes)) {
+		$current_dir  = 'bootswatch';
+		if (!file_exists($current_dir)) {
+			mkdir($current_dir, 0755, true);
+		}
+		if (file_exists($current_dir)) {
+			foreach($bootswatch->themes as $theme) {
+		    $target_dir = $current_dir . '/' . strtolower($theme->name);
+		    if (!file_exists($target_dir)) {
+		      mkdir($target_dir, 0755, true);
+		      exec("cd " . $target_dir . " && wget " . $theme->css);
+		      exec("cd " . $target_dir . " && wget " . $theme->cssMin);
+		    }
+			}
+		}
+	}
+}
